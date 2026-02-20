0ad67b3517348918f1418b332b769dc62baa1e21
Jason Dexter <jasondexter04@gmail.com>
2026-02-20 12:34:38 -0600

    Created Orchestrate.py to act as python's main. Comment added within to describe the python functionality needed.

---
+++ Orchestrate.py
@@ -0,0 +1,7 @@
+"""
+things to make:
+    Cluster Manager; interpret raft data
+    Policy Manager; determine where packets CANNOT go or who CANNOT join
+    Translator; turns intercepted processes to container spec [container preparation system]
+    Process filter; determine which processes SHOULDN'T be offloaded
+"""
