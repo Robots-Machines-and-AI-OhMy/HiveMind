b5f049abc843eee959e2a263179f98d8207a2001
nlobao <nicholas.lobao@ndus.edu>
2026-04-29 18:01:02 -0500

    more work on networking package
    yes, just a bit though

--- network.cpp
+++ network.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector> //basically array lists
 #include <chrono> //time keeper
+#include <winsock2.h> //networking package
 #include "network.hpp"
 #include "containerization.hpp" // handles process containerization logic
 
@@ -18,41 +19,6 @@ enum status {
     Follower,
     Candidate,
     None
-};
-
-class NetworkManager {
-
-    private:
-        const bool test; // for testing mode
-        status state = None;
-        double perfScore; // this device's performance score
-        Network currentNet = NULL; //this device's current network
-
-    public:
-        // creates a network with specified name and password
-        // password may be null
-        // returns true if network successfully created
-        bool createNetwork(string name, string password) {
-            //
-        }
-
-        // runs metric calculation algorithm
-        double calculateMetrics() {
-            return -1.0; //replace with algorithm later
-        }
-
-        // constructor, specifies testing mode
-        NetworkManager(bool testing) {
-            perfScore = calculateMetrics();
-            test = testing;
-        }
-
-        // default constructor, assumes testing mode false
-        NetworkManager() {
-            perfScore = calculateMetrics();
-            test = false;
-        }
-
 };
 
 class Network {
@@ -72,7 +38,7 @@ class Network {
         }
 
         //network with a name and password
-        Network(string netName, string pass, string leader) {
+        Network(string netName, string leader, string pass) {
             name = netName;
             password = pass;
             leadIP = leader;
@@ -91,4 +57,72 @@ class Network {
             else
                 return false;
         }
+};
+
+class NetworkManager {
+
+    private:
+        const bool test; // for testing mode
+        status state = None;
+        double perfScore; // this device's performance score
+        char* hostname = malloc(256 * sizeof(char));
+        winsock2::gethostname(hostname, 256);
+        Network currentNet = NULL; // this device's current network
+        // bind network names to IP addresses
+        // network join relies on this being up to date, collected from broadcast
+        vector routingTable;
+
+    public:
+        // creates a network with specified name and password
+        // password may be null
+        // returns true if network successfully created
+        bool createNetwork(string name, string password) {
+            if (password == NULL)
+                currentNet = new Network(name, hostname);
+            else
+                currentNet = new Network(name, hostname, password);
+
+            return true; //success
+        }
+
+        // disconnects this device from network
+        // returns true when complete
+        // returns false if an error was encountered
+        bool leaveNetwork() {
+            try {
+                //perform disconnection logic
+
+                //wipe network
+                Network currentNet = NULL;
+                return true;
+            }
+            catch (Exception e) {
+                return false;
+            }
+        }
+
+        // attempts to join network of specified name and password
+        // password may be null
+        // returns true if success, false if there was an error
+        bool joinNetwork(string name, string UID, string password) {
+            return false; //replace with join logic later
+        }
+
+        // runs metric calculation algorithm
+        double calculateMetrics() {
+            return -1.0; //replace with algorithm
+        }
+
+        // constructor, specifies testing mode
+        NetworkManager(bool testing) {
+            perfScore = calculateMetrics();
+            test = testing;
+        }
+
+        // default constructor, assumes testing mode false
+        NetworkManager() {
+            perfScore = calculateMetrics();
+            test = false;
+        }
+
 };
