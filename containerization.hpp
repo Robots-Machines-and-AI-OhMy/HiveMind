a991b4971a600a5a9c8e22b071da01d1eadb11c3
nlobao <nicholas.lobao@ndus.edu>
2026-04-28 20:53:17 -0500

    Create containerization.hpp

---
+++ containerization.hpp
@@ -0,0 +1,10 @@
+/*
+ * Handles logic for the containerization of processes
+ * for transmission over network
+ * Intended OS target: Windows
+ *
+ */
+
+#include "containerization.hpp"
+
+// code here
