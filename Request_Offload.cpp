6622ad54ec5266171a53084f08f651977c1e7980
Jason Dexter <jasondexter04@gmail.com>
2026-05-01 18:20:19 -0500

    Added some more backend files (not in use yet)

---
+++ Request_Offload.cpp
@@ -0,0 +1,7 @@
+//
+// Created by jason on 5/1/2026.
+//
+
+#include "Request_Offload.h"
+
+
---
+++ Request_Offload.h
@@ -0,0 +1,14 @@
+//
+// Created by jason on 5/1/2026.
+//
+
+#ifndef MINDMESH_REQUEST_OFFLOAD_H
+#define MINDMESH_REQUEST_OFFLOAD_H
+/*
+class Request_Engine {
+    public:
+    Engine();
+
+};
+*/
+#endif //MINDMESH_REQUEST_OFFLOAD_H
