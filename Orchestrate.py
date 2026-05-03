2d32d26a8d7c0a1c91e946cefe6a4cb935742ad1
Robots-Machines-and-AI-OhMy <Jasondexter04@gmail.com>
2026-05-02 22:43:29 -0500

    Delete Orchestrate.py
    removed unneccesary files

--- Orchestrate.py
+++
@@ -1,7 +0,0 @@
-"""
-things to make:
-    Cluster Manager; interpret raft data
-    Policy Manager; determine where packets CANNOT go or who CANNOT join
-    Translator; turns intercepted processes to container spec [container preparation system]
-    Process filter; determine which processes SHOULDN'T be offloaded
-"""
